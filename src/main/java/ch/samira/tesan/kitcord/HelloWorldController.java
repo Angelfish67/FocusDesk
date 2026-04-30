47094201f5888d3b6d44374c2f65719f263a0f82
angelfish67 <devcore404@gmail.com>
2026-04-30 10:46:47 +0200

    helloworld, not part of this project but stays for educational purposes

--- src/main/java/ch/samira/tesan/kitcord/HelloWorldController.java
+++ src/main/java/ch/samira/tesan/kitcord/HelloWorldController.java
@@ -1,4 +1,22 @@
 package ch.samira.tesan.kitcord;
 
+
+import lombok.Getter;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloWorldController {
+
+    @GetMapping("/helloworld")
+    public String helloworld(){
+        return "Hello World";
+    }
+
+    @GetMapping("/helloname")
+    public String helloname(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s", name);
+    }
+
 }
