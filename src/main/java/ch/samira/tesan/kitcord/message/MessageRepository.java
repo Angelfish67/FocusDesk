b96f2fa97382eb79eef7cf2ff050a76b8eb0cc5c
angelfish67 <devcore404@gmail.com>
2026-04-30 08:56:43 +0200

    add repos

---
+++ src/main/java/ch/samira/tesan/kitcord/message/MessageRepository.java
@@ -0,0 +1,6 @@
+package ch.samira.tesan.kitcord.message;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MessageRepository extends JpaRepository<Message, Long> {
+}
