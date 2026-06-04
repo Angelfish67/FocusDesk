d5ae2d25965e66aafbb006fe8262c61b14753b68
angelfish67 <devcore404@gmail.com>
2026-06-04 08:21:18 +0200

    Merge histories

--- src/main/java/ch/samira/tesan/kitcord/chat/enums/ChatType.java
+++
@@ -1,7 +0,0 @@
-package ch.samira.tesan.kitcord.chat.enums;
-
-
-public enum ChatType {
-    DIRECT,
-    GROUP,
-}
--- src/main/java/ch/samira/tesan/kitcord/security/Roles.java
+++
@@ -1,7 +0,0 @@
-package ch.samira.tesan.kitcord.security;
-
-public class Roles {
-    public static final String Admin = "admin";
-    public static final String Read = "read";
-    public static final String Update = "update";
-}
